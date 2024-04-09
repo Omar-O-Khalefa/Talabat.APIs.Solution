479206969f51262a2722561bd88b54652a99de8f
Omar Khalefa <Khalefa@DESKTOP-S9RHSS4>
2024-04-09 17:58:28 +0200

    Updating DataBase

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -7,7 +7,7 @@ namespace Talabat.APIs
 	public class Program
 	{
 		// Entrypoint
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var WebApplicationbuilder = WebApplication.CreateBuilder(args);
 
@@ -26,7 +26,27 @@ namespace Talabat.APIs
 			);
 			#endregion
 
+			#region Create DataBase For First Time Deploying
 			var app = WebApplicationbuilder.Build();
+
+			using var scope = app.Services.CreateScope();
+
+			var services = scope.ServiceProvider;
+
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			// Asking Clr For Createing Object From DbCOntext Explicitly
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				await _dbContext.Database.MigrateAsync(); // Update-DataBase
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "An Error Has been occured During apply the Migrations");
+			}
+
+			#endregion
 
 			#region Configure Kestrel Middlewares
 			// Configure the HTTP request pipeline.
