b5d41253424982e5e1d28353791c47b9fbae306a
Omar Khalefa <Khalefa@DESKTOP-S9RHSS4>
2024-05-12 09:49:57 +0300

    ImplmentUniteOfWork

---
+++ Talabat.Repository/UniteOfWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Entities.Product;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Infrastructure
+{
+    public class UniteOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _dbContext;
+
+        private Hashtable _repositories;
+
+        public UniteOfWork( StoreContext dbContext) //Ask Clr For Create Object From DbContext Iplicitly
+        {
+           _dbContext = dbContext;
+            _repositories = new Hashtable();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            var key = typeof(TEntity).Name; //Order
+            if (!_repositories.ContainsKey(key))
+            {
+                var repository = new GenericRepository<TEntity>(_dbContext) ;
+                _repositories.Add(key, repository);
+            }
+            return _repositories[key] as IGenericRepository<TEntity>;
+        }
+        public async Task<int> CompleteAsync()
+        {
+          return await _dbContext.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _dbContext.DisposeAsync();
+        }
+
+    }
+}
